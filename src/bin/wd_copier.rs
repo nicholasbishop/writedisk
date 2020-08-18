687f16da0c1f8ce7a033245f7d22d2b94934b702
Michael Pobega <pobega@gmail.com>
2020-08-18 17:40:08 -0400

    Add a progress bar to sync
    The progress bar is mostly a rough estimate of how much work there is.
    It checks the OS dirty pages before copying, compares it to the OS dirty
    pages after the copy and uses that as its baseline for completion to
    build a progress bar.

--- src/bin/wd_copier.rs
+++ src/bin/wd_copier.rs
@@ -2,6 +2,9 @@ use std::{
     fs,
     io::{Read, Write},
     path::PathBuf,
+    sync::mpsc::{self, TryRecvError},
+    thread,
+    time::Duration,
 };
 use structopt::StructOpt;
 
@@ -15,7 +18,11 @@ struct Opt {
 fn main() {
     let opt = Opt::from_args();
 
+    let meminfo = procfs::Meminfo::new().unwrap();
+    let starting_dirty = meminfo.dirty;
+
     let mut progress_bar = progress::Bar::new();
+    progress_bar.set_job_title("copying... (1/2)");
 
     let mut src = fs::File::open(opt.src).unwrap();
     let src_size = src.metadata().unwrap().len();
@@ -44,8 +51,30 @@ fn main() {
         bytes_written += read_size;
     }
 
-    println!("syncing...");
+    progress_bar.set_job_title("syncing... (2/2)");
+
+    let (tx, rx) = mpsc::channel();
+
+    let meminfo = procfs::Meminfo::new().unwrap();
+    let dirty_after_copy = meminfo.dirty - starting_dirty;
+
+    thread::spawn(move || loop {
+        let meminfo = procfs::Meminfo::new().unwrap();
+        let percent = 100
+            - ((meminfo.dirty.saturating_sub(starting_dirty))
+                / (dirty_after_copy / 100));
+        progress_bar.reach_percent(percent as i32);
+        thread::sleep(Duration::from_millis(500));
+        match rx.try_recv() {
+            Ok(_) | Err(TryRecvError::Disconnected) => {
+                break;
+            }
+            Err(TryRecvError::Empty) => {}
+        }
+    });
+
     dst.sync_data().unwrap();
+    tx.send(()).unwrap();
 
     println!("finished");
 }
